867b3d208a9d0141ef224c6b8caa6a8cd53fc196
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2023-03-24 19:01:54 +0100

    fix: pass eventValue to scroll and click tracker

--- src/modules/track.view.js
+++ src/modules/track.view.js
@@ -1,10 +1,10 @@
 import { createScrollTracking } from '@strg/behave-tracking-client-plugins'
 
-export const setupViewTracking = (getPushEvent, options) => (vm, eventKey, contentId) => {
+export const setupViewTracking = (getPushEvent, options) => (vm, eventKey, eventValue, contentId) => {
   const pushEvent = getPushEvent(eventKey || options.eventKeyView, contentId)
 
   const scrollTracking = createScrollTracking()
-  const visibilityMeter = scrollTracking.visibility(vm.$el, {})
+  const visibilityMeter = scrollTracking.visibility(vm.$el, { eventValue })
   scrollTracking.on('visibility', ({ value }) => pushEvent(value))
 
   vm.$once('hook:destroyed', () => visibilityMeter.unbind())
